38b2a221d365449dc266a4cd16a2b031855bd51a
phallyn-chean <phollyn.chean@student.cadt.edu.kh>
2023-09-04 09:49:11 +0700

    initialize todo app firebase

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
